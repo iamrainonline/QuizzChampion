0daaca20347a1f7818abde03ffe3b8543d2d9ac3
unknown <cristiancandidatu@hotmail.com>
2024-03-29 15:39:33 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+const corsConfig = {
+   origin: ["http://localhost:3000"],
+   credentials: true,
+   methods: "GET,HEAD,OPTIONS,PUT,POST,DELETE, PATCH",
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(8800, () => {
+   console.log("connected express PORT:", 8800, " [Success]");
+});
